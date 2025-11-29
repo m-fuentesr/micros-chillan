b5a124e1f516d78ac42560746e2024dc2e3feb5b
SorkoTheProgram <ig.sorko@duocuc.cl>
2025-11-29 06:16:32 -0300

    Se creo: "test_login.py" para sacar el token y poder funcionar la terminal con eso. Se agrego: 1.-el schema para el perfil del trabajador en "driver.py". 2.-La funcion para llamar los datos del perfil del trabajador a excepcion de las estadisticas del mes en "driver_service.py". 3.-  Se agrego el router correctamente en "worker.py" para la conexion.

--- backend/app/schemas/driver.py
+++ backend/app/schemas/driver.py
@@ -79,3 +79,18 @@ class DriverListResponse(BaseModel):
     """
     items: list[DriverListItem]
 
+
+
+class WorkerProfileResponse(BaseModel):
+    # Cabecera
+    nombre_completo: str
+    
+    # Información Personal
+    rut: str
+    telefono: str
+    email: str
+    
+    # Información Laboral
+    maquina_detalle: Optional[str] # Ej: "MÁQUINA 01 - Mercedes Benz"
+    fecha_ingreso: str             # Ej: "20-11-2024"
+    
