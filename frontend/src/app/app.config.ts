038f85449d002ab04c664c609feff88cca2afde7
m-fuentesr <163899771+m-fuentesr@users.noreply.github.com>
2025-12-01 05:02:23 -0300

    fix(app.config): reorganizar importaciones y arreglar la configuración de los router

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,24 +1,24 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { routes } from './app.routes';
-import { authInterceptor } from './shared/interceptors/auth.interceptor';
 import { provideRouter, withPreloading } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { authInterceptor } from './shared/interceptors/auth.interceptor';
 import { SmartPreloadingStrategy } from './core/strategies/smart-preloading.strategy';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+
+    provideRouter(
+      routes,
+      withPreloading(SmartPreloadingStrategy),
+    ),
+
     provideHttpClient(
       withInterceptors([authInterceptor])
-    )
-    provideRouter(routes, withPreloading(SmartPreloadingStrategy)),
-    provideHttpClient(),
+    ),
     SmartPreloadingStrategy
   ]
 };
