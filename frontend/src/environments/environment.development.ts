b9148e2b32f882ad95ce62e8e081092c6f1f3d4d
Benjamin Rebolledo <cjentus.benjamin@gmail.com>
2026-01-06 01:19:05 -0300

    feat: Introduce environment configuration files for production and development.

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8000',
-  supabaseUrl: 'https://whakzffihcapzrlqjdgp.supabase.co',
-  supabaseAnonKey: 'sb_publishable_wx0hxbnkkUgw_BETDIQX-Q_7LUIAFKA',
+  apiBaseUrl: 'https://micros-chillan-production.up.railway.app',
+  supabaseUrl: 'https://vwepdhtfdngowxizmzzz.supabase.co',
+  supabaseAnonKey: 'sb_publishable_70dWeSYQZajr3jSI1PKEiw_YraFtHjp',
 };
-// apiBaseUrl: 'https://micros-chillan-production.up.railway.app'
