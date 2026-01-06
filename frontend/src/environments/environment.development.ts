dc27a5fbc75ea267c7d87b463bdb9f5ae36c0445
m-fuentesr <163899771+m-fuentesr@users.noreply.github.com>
2026-01-06 00:03:04 -0300

    env actualizadas

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'https://micros-chillan-production.up.railway.app',
-  supabaseUrl: 'https://whakzffihcapzrlqjdgp.supabase.co',
-  supabaseAnonKey: 'sb_publishable_wx0hxbnkkUgw_BETDIQX-Q_7LUIAFKA',
+  supabaseUrl: 'https://vwepdhtfdngowxizmzzz.supabase.co',
+  supabaseAnonKey: 'sb_publishable_70dWeSYQZajr3jSI1PKEiw_YraFtHjp',
 };
 // apiBaseUrl: 'https://micros-chillan-production.up.railway.app'
